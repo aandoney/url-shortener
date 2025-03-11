5ab936d6ba5475271984e28e857c309990f5c3a9
aandoney <angel_andoney@live.com.mx>
2025-03-11 15:24:29 -0600

    deploy database

--- Api/src/UrlShortener.Api/Program.cs
+++ Api/src/UrlShortener.Api/Program.cs
@@ -1,4 +1,7 @@
+using Azure.Core;
 using Azure.Identity;
+using UrlShortener.Api.Extensions;
+using UrlShortener.Core.Urls.Add;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +11,14 @@ if (!string.IsNullOrEmpty(keyVaultName))
     builder.Configuration.AddAzureKeyVault(
         new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
         new DefaultAzureCredential()
-    );    
+    );
 }
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSingleton(TimeProvider.System);
+builder.Services.AddUrlFeature();
 
 var app = builder.Build();
 
@@ -25,28 +30,25 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapPost("/api/urls",
+    async (AddUrlHandler handler,
+        AddUrlRequest request,
+        CancellationToken cancellationToken) =>
+    {
+        var requestWithUser = request with
+        {
+            CreatedBy = "aandoney@test.com"
+        };
+        var result = await handler.HandleAsync(requestWithUser, cancellationToken);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+        if (!result.Succeeded)
+        {
+            return Results.BadRequest(result.Error);
+        }
+
+        return Results.Created(
+            $"/api/urls/{result.Value!.ShortUrl}",
+            result.Value);
+    });
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
